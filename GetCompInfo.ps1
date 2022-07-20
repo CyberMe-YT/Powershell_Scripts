49a53b17f6b4f438987782abb6134a148e237752
Jack <83857608+CyberMe-Jack@users.noreply.github.com>
2022-07-20 18:40:33 +0900

    Initial Commit
    Get computer info for build/version/OS type. Used quickly grab info for machines on domain.

---
+++ GetCompInfo.ps1
@@ -0,0 +1,14 @@
+﻿#################################################################
+# Purpose: Get various computer info from domain                #
+#                                                               #
+#################################################################
+# Parameters
+param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [ValidateSet('osbuildnumber','OSType','OsVersion')]
+    [string]$selectob
+    )
+# Get Computer Info for all computers
+$ErrorActionPreference = 'silentlycontinue'
+$computers = Get-ADComputer -filter * | Select -ExpandProperty name
+$cinfo = Invoke-Command -ComputerName $computers -ScriptBlock { get-computerinfo } | Select PSComputerName,$selectob
+$cinfo
