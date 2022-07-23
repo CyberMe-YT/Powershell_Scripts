3a8d3d9d541a08983a43cb3aaeccfc2e791b9f87
Jack <83857608+CyberMe-Jack@users.noreply.github.com>
2022-07-23 18:18:10 +0900

    Initial Commit of password warning (server/client)

---
+++ PassExpireCheck(client).ps1
@@ -0,0 +1,13 @@
+﻿# Get current user logged in
+$cuser = $env:USERNAME
+$regex = ''
+# Read file and compare $cuser to get true/false for each
+foreach($line in Get-Content -Path "\\10.0.0.2\testShare\Password Check scripts\UsersExpire.txt"){
+    if($line -match $regex){
+        if($line -match $cuser){
+            # Warning Prompt
+powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('Account Expiration in less then 30 days. Contact office. ','WARNING')}"
+        }
+    }
+        
+}
---
+++ PasswordWarning(server).ps1
@@ -0,0 +1,30 @@
+﻿
+# Test path to UsersExpire.txt, delete if exists
+$file = 'C:\Users\admin\Documents\UsersExpire.txt'
+            # Test if path exists, delete if so
+            if (Test-Path -Path  $file){
+                Remove-Item $file
+                # Create blank file
+                $null = New-Item $file 
+            }
+
+# Get users
+$users = Get-ADUser -filter * | select -ExpandProperty SamAccountName
+# Boolean, get todays date + 30 if gt then edate prompt user
+$tdate = (get-date)
+$accexpire = [DateTime]$edate -gt $tdate
+# Loop through each object, append any true responses
+ForEach ($user in $users){
+    # Get account expiration date
+    $edate = Get-ADUser -identity $user -Properties * | select -ExpandProperty AccountExpirationDate
+    # Filter null expiration dates
+    if (!($edate -eq $null)){
+        # Is acc expire less then 30 days from current date
+        $accexpire = ($edate - $tdate | select days)
+
+        if ($accexpire.Days -lt 30){
+            # Write hostname to textfile
+            Add-Content $file $user
+        }
+    }    
+    }
