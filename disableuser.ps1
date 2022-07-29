97ad7b2e64b95b00741c88fcb2969ff4a8598965
Jack <83857608+CyberMe-Jack@users.noreply.github.com>
2022-07-29 19:38:23 +0900

    Initial offboard script

---
+++ disableuser.ps1
@@ -0,0 +1,13 @@
+﻿# Ask for user to disable
+$udisable = Read-Host "Enter username:"
+# Verify user exists, error handle
+$User = $(try{get-aduser $udisable} catch {$null})
+
+if ($User -ne $Null) {
+    Disable-ADAccount -Identity $udisable
+    Get-Aduser -Identity $udisable | Select-Object SamAccountName, Enabled
+
+} Else {
+    Write-Host "User does not exist"
+    exit
+}
